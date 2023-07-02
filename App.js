969ec107ebf35058d4ba568a48a697179ad19768
Semih Yücel <65440304+semihycl2397@users.noreply.github.com>
2023-07-02 13:45:22 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,29 @@
+import React, { useState } from 'react';
+import ShoesModal from './components/ShoesModal';
 
+const App = () => {
+  const [shoeCount, setShoeCount] = useState(70);
+  const [donationCount, setDonationCount] = useState(36);
+
+  const handleShoeChange = (newShoes) => {
+    setShoeCount(newShoes + donationCount);
+  };
+
+  const handleDonationChange = (newDonations) => {
+    setDonationCount(newDonations);
+  };
+
+  return (
+    <div>
+      <h1>Ayakkabi Uygulamasi</h1>
+      <ShoesModal
+        shoeCount={shoeCount}
+        donationCount={donationCount}
+        onShoeChange={handleShoeChange}
+        onDonationChange={handleDonationChange}
+      />
+    </div>
+  );
+};
+
+export default App;
